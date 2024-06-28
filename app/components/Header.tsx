8101e818d52b69af3bdc782fb953ed24fe86b193
kyndrawynne <knwynne@asu.edu>
2024-06-28 02:11:42 -0700

    Creates and sets up header styling.

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,14 @@
+import styles from './Header.module.css'
+
 export default function Header() {
-    return (
-      <header>
-        {/* Add your header content here */}
-      </header>
-    )
-  }
+  return (
+    <header className={styles.header}>
+      <nav>
+        <ul className={styles.nav}>
+          <li><a href="#about">About</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
